7953080549c58d946114bd13de0109838a04df10
Enes Akın <72943799+enesakin1@users.noreply.github.com>
2020-12-18 08:03:26 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import React from "react";
 import Firebase, { FirebaseProvider } from "./config/Firebase";
 import AppContainer from "./navigation/navigation";
 import { NavigationContainer } from "@react-navigation/native";
+import { YellowBox } from "react-native";
+YellowBox.ignoreWarnings(["Setting a timer"]);
+YellowBox.ignoreWarnings(["YellowBox"]);
 export default function App() {
   return (
     <FirebaseProvider value={Firebase}>
